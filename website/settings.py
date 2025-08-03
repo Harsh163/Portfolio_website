e6f6cf8a61ade44afcf67d1f7ba9af18dcdccd81
Harsh163 <harshhingu016@gmail.com>
2025-08-03 06:05:58 +0100

    Add staticfiles for Render deployment 2.o

--- website/settings.py
+++ website/settings.py
@@ -137,7 +137,6 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static')]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/staticfiles')
 
 # Default primary key field type
