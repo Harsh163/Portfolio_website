e17b1d534eac4e8da2b505fca1706c516dd57fef
Harsh163 <harshhingu016@gmail.com>
2025-08-03 06:36:32 +0100

    Fix static file config for Render.2.o

--- website/settings.py
+++ website/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
