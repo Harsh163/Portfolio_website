b0df1aaa04778b09f9ff5edaa9d6512f272f7f29
Harsh163 <harshhingu016@gmail.com>
2025-08-03 05:33:51 +0100

    Add staticfiles for Render deployment

--- website/settings.py
+++ website/settings.py
@@ -30,14 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
-
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ['harshhingu.onrender.com', 'localhost', '127.0.0.1']
-
 
 
 # Application definition
@@ -102,10 +100,6 @@ DATABASES = {
 }
 
 
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -143,6 +137,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
